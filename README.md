5ed2e900bf76fb10d3eec8ce8f5c9a2f3afbe929
Cinzya <cynti-ebert@web.de>
2021-10-04 16:48:41 +0200

    UPDATE :: refactor for Vuetify support

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# LoL Producer
-Electron App to interact with the League of Legends Client for broadcast purposes.
+# vue-electron-app
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
-createApp(App).use(store).use(router).mount("#app");
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
