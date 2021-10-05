7d33bbdfb335daee55fb29acb95cdef3ca2a4bde
Cinzya <cynti-ebert@web.de>
2021-10-05 17:59:12 +0200

    UPDATE :: add try catch

--- src/mixin.js
+++ src/mixin.js
@@ -5,25 +5,23 @@ export default {
   },
     methods: {
       requestLCU: async function(endpoint) {
-        console.log("Fetch Data...");
-        let basestring = `${this.LCU.username}:${this.LCU.password}`;
-        let buff = new Buffer(basestring);
-        let base64string = buff.toString('base64');
-        await fetch(`${this.LCU.protocol}://${this.LCU.address}:${this.LCU.port}${endpoint}`,{
-            method: 'GET',
-            headers: {
-                'Authorization': `Basic ${base64string}`,
-                'Accept': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            return data;
-        })
-        .catch((error) => {
+          console.log("Fetch Data...");
+          let basestring = `${this.LCU.username}:${this.LCU.password}`;
+          let buff = new Buffer(basestring);
+          let base64string = buff.toString('base64');
+
+          try {
+            let response = await fetch(`${this.LCU.protocol}://${this.LCU.address}:${this.LCU.port}${endpoint}`,{
+                method: 'GET',
+                headers: {
+                    'Authorization': `Basic ${base64string}`,
+                    'Accept': 'application/json'
+                }
+            })
+            return await response.json();
+          } catch (error) {
             console.error('Error:', error);
-        });
+          }
       }
     }
 };
