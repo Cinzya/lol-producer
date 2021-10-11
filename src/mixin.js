abe23357895fde4c0471c919727de373f341e598
Cinzya <cynti-ebert@web.de>
2021-10-11 06:27:18 +0200

    RM :: LCU requests

--- src/mixin.js
+++ src/mixin.js
@@ -1,31 +1,4 @@
-import { mapState } from "vuex";
-
 export default {
-  computed: {
-    ...mapState(["LCU"]),
-  },
-  methods: {
-    requestLCU: async function (endpoint) {
-      console.log("Fetch Data...");
-      let basestring = `${this.LCU.username}:${this.LCU.password}`;
-      let buff = new Buffer(basestring);
-      let base64string = buff.toString("base64");
-
-      try {
-        let response = await fetch(
-          `${this.LCU.protocol}://${this.LCU.address}:${this.LCU.port}${endpoint}`,
-          {
-            method: "GET",
-            headers: {
-              Authorization: `Basic ${base64string}`,
-              Accept: "application/json",
-            },
-          }
-        );
-        return await response.json();
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    },
-  },
+  computed: {},
+  methods: {},
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import LCUConnector from "lcu-connector";
-const connector = new LCUConnector();
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    LCU: {},
-  },
-  mutations: {
-    async getLogins(state) {
-      await connector.on("connect", (data) => {
-        state.LCU = data;
-        //  {
-        //    address: '127.0.0.1'
-        //    port: 18633,
-        //    username: 'riot',
-        //    password: H9y4kOYVkmjWu_5mVIg1qQ,
-        //    protocol: 'https'
-        //  }
-      });
-      // Start listening for the LCU client
-      connector.start();
-    },
-  },
-  actions: {
-    async connectLCU({ commit }) {
-      await commit("getLogins");
-    },
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {},
 });
